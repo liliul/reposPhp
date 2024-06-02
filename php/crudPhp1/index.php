9000bf6dbacb711ee688015ca895f697362ab955
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-02 19:11:54 -0300

    feat: index.php crudPhp1 php

--- php/crudPhp1/index.php
+++ php/crudPhp1/index.php
@@ -7,7 +7,71 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container my-5">
+        <h2>Lista de Clientes</h2>
+        <a class="btn btn-primary" href="/myshop/create.php" role="button">Novo cliente</a>
+        
+        <br>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Endereço</th>
+                    <th>Data</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php 
+                    $serverName = "localhost";
+                    $userName = "root";
+                    $password = "";
+                    $database = "myshop";
+
+                    // criar conecxão
+                    $connection = new mysqli($serverName, $userName, $password, $database);
+
+                    // checando conecxão
+                    if ($connection->connect_error) {
+                        die("Connection failed: " . $connection->connect_error);
+                    }
+                    
+                    // lendo banco de dados
+                    $sql = "SELECT * FROM clientsMyshop";
+                    $result = $connection->query($sql);
+
+                    if(!$result) {
+                        die("Invalid query: " . $connection->error);
+                    }
+
+                    while($row = $result->fetch_assoc()) {
+                        echo "
+                        <tr>
+                            <td>$row[id]</td>
+                            <td>$row[name]</td>
+                            <td>$row[email]</td>
+                            <td>$row[phone]</td>
+                            <td>$row[address]</td>
+                            <td>$row[create_at]</td>
+                            <td>
+                                <a class='btn btn-primary btn-sm' href='/myshop/edit.php?id=$row[id]'>Editar</a>
+                                <a class='btn btn-danger btn-sm' href='/myshop/del.php?id=$row[id]'>Delete</a>
+                            </td>
+                        </tr>
+                        ";
+                    }
+
+
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
