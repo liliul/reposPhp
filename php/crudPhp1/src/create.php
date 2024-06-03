35758f9f3555ce283194cb47e3d08beeb2767178
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-02 21:10:06 -0300

    fix: função do php header no create.php crudPhp1 php

---
+++ php/crudPhp1/src/create.php
@@ -0,0 +1,135 @@
+<?php
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $database = "myshop";
+
+    // criar conecxão
+    $connection = new mysqli($serverName, $userName, $password, $database);
+
+    $name = "";
+    $email = ""; 
+    $phone = ""; 
+    $address = ""; 
+
+    $errorMessage = "";
+    $successMessage = "";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $address = $_POST["address"];
+
+        do {
+            if (empty($name) || empty($email) || empty($phone) || empty($address)) {
+                $errorMessage = "All the field are required";
+                break;
+            }
+
+            // adicionar novo cliente no banco de dados
+            $sql = "INSERT INTO clientsMyshop (name, email, phone, address)" . "VALUES ('$name', '$email', '$phone', '$address')";
+            $result = $connection->query($sql);
+
+            if(!$result) {
+                die("Invalid query: " . $connection->error);
+            }
+
+            $name = "";
+            $email = ""; 
+            $phone = ""; 
+            $address = ""; 
+            
+            $successMessage = "Client added cirrectly";
+
+            header("location: ../index.php");
+            exit;
+
+        } while (false);
+    }
+   
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>Novo Cliente</h2>
+
+        <?php
+            if (!empty($errorMessage)) {
+                echo "
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong>$errorMessage</strong>
+                    <button class='btn-close' type='button' data-bs-alert='alert' aria-label='close'></button>
+                </div>
+                ";
+            }
+        ?>
+
+        <form action="" method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Nome</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name;?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="<?php echo $email;?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone;?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Endereço</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address;?>">
+                </div>
+            </div>
+
+            <?php
+                if(!empty($successMessage)) {
+                    echo "
+                    <div class='row mb-3'>
+                        <div class='offset-sm-3 col-sm-6'>
+                            <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                                <strong>$successMessage</strong>
+                                <button class='btn-close' type='button' data-bs-alert='alert' aria-label='Close'></button>
+                            </div>
+                        </div>
+                    </div>
+                    ";
+                }
+            ?>
+            
+            <div class="row mb-3">
+               <div class="offset-sm-3 col-sm-3 d-grid">
+                <button type="submit" class="btn btn-primary">Enviar</button>  
+               </div> 
+               <div class="col-sm-3 d-grid">
+                <a class="btn btn-outline-primary" href="/myshop/index.php" role="button">Cancelar</a>
+               </div>
+            </div>
+        </form>
+    </div>
+                
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
