975a44c125d60783524eab55a894062a45113d66
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-07-27 22:09:00 -0300

    chore: conexão com mysql e mudando nome de arquivo todolist

--- php/todolist/db.php
+++ php/todolist/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = "172.17.0.2";
 $username = "root";
-$password = ""; // senha padrão, ajuste conforme necessário
-$dbname = "todo_db";
+$password = "root";
+$dbname = "todolistDB";
 
 // Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
