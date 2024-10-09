f59ac4850bb8319385794b6a07ae56c2dfa161a0
liliul <liliuestudo@gmail.com>
2024-10-08 21:09:23 -0300

    add: style css laravel/todolist

--- php/projetosLaravel/todolist/resources/views/tasks/index.blade.php
+++ php/projetosLaravel/todolist/resources/views/tasks/index.blade.php
@@ -2,27 +2,32 @@
 <html>
 <head>
     <title>To-Do List</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-<body>
-    <h1>To-Do List</h1>
+<body class="body-todolist">
+    <section class="container-todolist">
+        <h1>To-Do List</h1>
 
-    <form action="{{ route('tasks.store') }}" method="POST">
-        @csrf
-        <input type="text" name="title" placeholder="Nova tarefa" required>
-        <button type="submit">Adicionar</button>
-    </form>
+        <div class="todolist-center">
+            <form action="{{ route('tasks.store') }}" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="Nova tarefa" required>
+                <button type="submit">Adicionar</button>
+            </form>
 
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-                {{ $task->title }}
-                <form action="{{ route('tasks.destroy', $task) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Remover</button>
-                </form>
-            </li>
-        @endforeach
-    </ul>
+            <ul>
+                @foreach ($tasks as $task)
+                    <li>
+                        {{ $task->title }}
+                        <form action="{{ route('tasks.destroy', $task) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Remover</button>
+                        </form>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </section>
 </body>
 </html>
